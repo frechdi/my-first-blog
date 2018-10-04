6e819509064b770d3871b0c5f4a6695b2737a1bc
= <xenotronic@hotmail.com>
2018-10-04 14:01:30 +0200

    first commit

---
+++ Scripts/django-admin.py
@@ -0,0 +1,5 @@
+#!c:\users\admin\google drive\learning and study\programming\django\scripts\python.exe
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
